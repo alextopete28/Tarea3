e6fe30dcea14c335a401999c4a266d2cb0c39af4
Alejandro Topete <atopete@Alejandros-Mac.local>
2017-09-20 12:21:44 -0700

    Initial Commit

---
+++ Tarea3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tarea3
+//
+//  Created by Alejandro Topete on 9/20/17.
+//  Copyright © 2017 AVALTO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tarea3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tarea3
+//
+//  Created by Alejandro Topete on 9/20/17.
+//  Copyright © 2017 AVALTO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
